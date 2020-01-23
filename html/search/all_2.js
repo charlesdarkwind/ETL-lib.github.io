e15f7ca1f296728ccf094d440f885e7f849e2303
charlesdarkwind <jasmin.parent@bnc.ca>
2020-01-23 15:04:24 -0500

    Auto-doc update

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['build_5fpath_7',['build_path',['../namespace_e_t_l_1_1utils.html#a8473d064acef9209402a2fb942cef57a',1,'ETL::utils']]],
-  ['build_5fregex_5fpart_8',['build_regex_part',['../namespace_e_t_l_1_1raw__control.html#acad4995563caf1d4613396a939618dcc',1,'ETL::raw_control']]]
+  ['build_5fregex_5fpart_8',['build_regex_part',['../namespace_e_t_l_1_1raw__control.html#a68c01ed82bea7ebc7cea7c18c06e96ac',1,'ETL::raw_control']]]
 ];
