9c7884435027312bb14aa2c5c547cf27dfca7e7f
charlesdarkwind <jasmin.parent@bnc.ca>
2020-03-02 12:42:52 -0500

    Auto-doc update

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['merge_89',['merge',['../namespace_e_t_l_1_1curated__tables.html#a30afc3c3aca3346b37b8eb368f3b2cd7',1,'ETL.curated_tables.merge()'],['../namespace_e_t_l_1_1delta__utils.html#a1b5382c97de7abcc49f5cf213f91656d',1,'ETL.delta_utils.merge()']]],
+  ['merge_89',['merge',['../namespace_e_t_l_1_1curated__tables.html#a40dae4a61a653ae9a870b2fad25e19c1',1,'ETL.curated_tables.merge()'],['../namespace_e_t_l_1_1delta__utils.html#a1b5382c97de7abcc49f5cf213f91656d',1,'ETL.delta_utils.merge()']]],
   ['message_90',['message',['../class_e_t_l_1_1exceptions_1_1_no_tables_defined_exception.html#a52154ba17f7b46acc9da8ee0163baacc',1,'ETL::exceptions::NoTablesDefinedException']]],
   ['migrate_5flegacy_5fcontrol_5ftable_91',['migrate_legacy_control_table',['../namespace_e_t_l_1_1raw__control.html#af869949247d74f2fdc36531a9f62235b',1,'ETL::raw_control']]],
   ['mount_5fis_5faccessible_92',['mount_is_accessible',['../namespace_e_t_l_1_1config.html#a835a86eb14dfc53363e9798c29647687',1,'ETL::config']]],
--- html/search/functions_b.js
+++ html/search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['merge_211',['merge',['../namespace_e_t_l_1_1curated__tables.html#a30afc3c3aca3346b37b8eb368f3b2cd7',1,'ETL.curated_tables.merge()'],['../namespace_e_t_l_1_1delta__utils.html#a1b5382c97de7abcc49f5cf213f91656d',1,'ETL.delta_utils.merge()']]],
+  ['merge_211',['merge',['../namespace_e_t_l_1_1curated__tables.html#a40dae4a61a653ae9a870b2fad25e19c1',1,'ETL.curated_tables.merge()'],['../namespace_e_t_l_1_1delta__utils.html#a1b5382c97de7abcc49f5cf213f91656d',1,'ETL.delta_utils.merge()']]],
   ['migrate_5flegacy_5fcontrol_5ftable_212',['migrate_legacy_control_table',['../namespace_e_t_l_1_1raw__control.html#af869949247d74f2fdc36531a9f62235b',1,'ETL::raw_control']]],
   ['mount_5fis_5faccessible_213',['mount_is_accessible',['../namespace_e_t_l_1_1config.html#a835a86eb14dfc53363e9798c29647687',1,'ETL::config']]],
   ['mount_5fzone_214',['mount_zone',['../class_e_t_l_1_1config_1_1_config.html#a2f8205999ea71adf8b047cfdc2b27232',1,'ETL::config::Config']]],
